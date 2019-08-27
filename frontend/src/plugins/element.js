99aba1fcb1fcbd911934ec21902ddc9119bdf9a4
qingcaier <1721119157@qq.com>
2019-08-27 13:06:59 +0800

    导航栏更新

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,4 +1,24 @@
-import Vue from 'vue'
-import { Button } from 'element-ui'
+import Vue from "vue";
+import {
+  Button,
+  Row,
+  Col,
+  Menu,
+  MenuItem,
+  Submenu,
+  Icon,
+  Form,
+  FormItem,
+  Input
+} from "element-ui";
 
-Vue.use(Button)
+Vue.use(Button);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Submenu);
+Vue.use(Icon);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,23 +1,36 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+// import EditNote from "./views/EditNote.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
+      id: "0"
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/edit",
+      name: "edit",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Edit.vue")
+    },
+    {
+      path: "/drafts",
+      name: "drafts",
+      component: () => import("./views/Drafts.vue")
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("./views/Register.vue")
     }
   ]
-})
+});
